676141eb4a7770fe51cc5da1c1a623ee0742a2d6
lettas <lettas0726@gmail.com>
2018-04-24 08:54:20 +0900

    Handをガガガッと実装

---
+++ Sources/Blackjack/Hand.swift
@@ -0,0 +1,31 @@
+class Hand {
+    private var cards = [Card]()
+
+    var score: Int {
+        // 手札が取りうるスコアの中から、21以下で一番大きい数を返す
+        let score = scores.sorted().reversed().first(where: { $0 <= 21 })
+        // 21以下がなかったらなんでもいいので一番小さい数を返す
+        return score ?? scores.first!
+    }
+
+    var scores: [Int] {
+        var scores = [0]
+
+        for card in cards {
+            // card.numberが1の時はは1か11かを選ぶことができる
+            if card.number == 1 {
+                scores = scores.map { $0 + 1 } + scores.map { $0 + 11 }
+            }
+            else {
+                let cardScore = min(card.number, 10)
+                scores = scores.map { $0 + cardScore }
+            }
+        }
+
+        return scores
+    }
+
+    func add(card: Card) {
+        self.cards = self.cards + [card]
+    }
+}
